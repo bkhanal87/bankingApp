35d1433be0788d4a9377cccdad86b894363b31d8
bkhanal87@gmail.com <bkhanal87@gmail.com>
2023-05-14 00:40:46 -0400

    create main class and object

---
+++ src/bankApp/BankingApp.java
@@ -0,0 +1,8 @@
+package bankApp;
+
+public class BankingApp {
+    int accountNumber = 0;
+    String accountHolder = "";
+    int phoneNumber = 0;
+    int lastFourSsn = 0;
+}
---
+++ src/bankApp/User.java
@@ -0,0 +1,8 @@
+package bankApp;
+
+public class User {
+    public static void main(String[] args) {
+        BankingApp u = new BankingApp();
+        System.out.println(u.accountNumber);
+    }
+}
